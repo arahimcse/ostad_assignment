90e66cc29d1ce8cb620b7b55bead6e9b749733de
Abdur Rahim <arahimcse@gmail.com>
2023-07-16 21:48:47 +0600

    Assignment 1 Module 3

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import SiteHeader from './components/siteHeader.vue';
+import homeSection from './components/homeSection.vue';
+import FeaturesSection from './components/featuresSection.vue';
+import PageSectionbgl1 from './components/pageSectionbgl1.vue';
+import pageScetion1 from './components/pageScetion1.vue';
+import PricingSection from './components/pricingSection.vue';
+import PageSection2 from './components/pageSection2.vue';
+import TestimonialSection from './components/testimonialSection.vue';
+import AboutSection from './components/aboutSection.vue';
+import PageSectionbgl2 from './components/pageSectionbgl2.vue';
+import ContactSection from './components/contactSection.vue';
+import SiteFooter from './components/siteFooter.vue';
+</script>
+
+<template>
+  <SiteHeader />
+  <homeSection />
+  <FeaturesSection />
+  <PageSectionbgl1 />
+  <pageScetion1 />
+  <PricingSection />
+  <PageSection2 />
+  <TestimonialSection />
+  <AboutSection />
+  <PageSectionbgl2 />
+  <ContactSection />
+  <SiteFooter />
+</template>
+
+<style scoped></style>
