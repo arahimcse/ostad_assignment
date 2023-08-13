4378799908a9d270d1d09f5cea7b093b6a270dfa
Abdur Rahim <arahimcse@gmail.com>
2023-08-13 12:23:30 +0600

    Assignment 1 Module 7

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Carousel from './components/Carousel.vue';
+import PieChart from './components/PieChart.vue';
+
+
+</script>
+
+<template>
+<!-- Task 1 -->
+<Carousel/>
+<!-- Task 2: -->
+<PieChart/>
+</template>
+
+<style scoped>
+  
+</style>
